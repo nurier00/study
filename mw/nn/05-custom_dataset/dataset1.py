f6ddcc48e037ce37dcc64d1b1b503466434467c8
nurier00 <76939970+nurier00@users.noreply.github.com>
2021-02-01 15:54:18 +0900

    add lab

--- mw/nn/05-custom_dataset/dataset1.py
+++ mw/nn/05-custom_dataset/dataset1.py
@@ -4,7 +4,4 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_breast_cancer
-cancer = load_breast_cancer()
 
-df = pd.DataFrame(cancer.data, columns=cancer.feature_names)
-df['class'] = cancer.target
